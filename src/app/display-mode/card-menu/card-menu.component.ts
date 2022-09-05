6fb2ba54d6ed39a6462016674b956b323524a472
sara <sanatdoosts@gmail.com>
2022-09-05 14:40:38 +0430

    use interface for cards in cardmenu

--- src/app/display-mode/card-menu/card-menu.component.ts
+++ src/app/display-mode/card-menu/card-menu.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { periodicEelement } from 'src/app/menu/interface/periodicElement';
+import {MatTableDataSource} from '@angular/material/table';
 
 @Component({
   selector: 'app-card-menu',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardMenuComponent implements OnInit {
 
+  elements: periodicEelement[]= [
+    {imageUrl: "https://material.angular.io/assets/img/examples/shiba2.jpg", name: 'چیستا محمدی', id: 100256, role: 'حسابدار' ,date: '1400/12/01' ,email:'maggi45@gmail.com' ,lastActivity:'امروز - 10:23'},
+    {imageUrl: "https://material.angular.io/assets/img/examples/shiba2.jpg", name: 'چیستا محمدی', id: 100256, role: 'حسابدار' ,date: '1400/12/01' ,email:'maggi45@gmail.com' ,lastActivity:'امروز - 10:23'},
+  ]
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  dataSource = new MatTableDataSource<periodicEelement>(this.elements);
 }
